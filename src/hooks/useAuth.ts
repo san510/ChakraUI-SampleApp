cfd9cdce7602fe1e9b9de02e31351e588af46206
goto <goto@avancesys.co.jp>
2022-10-23 11:59:32 +0900

    add ShowMessage

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,30 +3,37 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
+import { title } from "process";
 
 export const useAuth = () => {
   const navitage = useNavigate();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
-  const login = useCallback((id: string) => {
-    setLoading(true);
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true);
 
-    axios
-      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        if (res.data) {
-          navitage("/home");
-        } else {
-          alert("ユーザーが見つかりません");
-        }
-      })
-      .catch(() => {
-        alert("ログインできません");
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
+            navitage("/home");
+          } else {
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
+          }
+        })
+        .catch(() => {
+          showMessage({ title: "ログインできません", status: "error" });
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [navitage, showMessage]
+  );
   return { login, loading };
 };
